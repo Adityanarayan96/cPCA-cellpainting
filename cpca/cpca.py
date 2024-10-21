a085677e6dc5f433a55e9b42b58f76ff67ba01e6
Adityanarayan96 <adityanarayanoct1997@gmail.com>
2024-10-21 11:49:56 -0500

    "Added extract_controls function to ContrasticePCA class"

--- cpca/cpca.py
+++ cpca/cpca.py
@@ -1,8 +1,10 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import sys
+import functools
 sys.path.append("..")
 import logging
 from logging_config import setup_logging
+import pandas as pd
 logger = logging.getLogger(__name__)
 
 class ContrastivePCA:
@@ -16,6 +18,7 @@ class ContrastivePCA:
         Constructs necessary attributes (hyperparameters) for ContrastivePCA
 
         Parameters:
+        ===========
         alpha : contrastive constant, can be set or found through grid search
         num_components : number of components to retain, will update to weights in the future, can be set or found through grid search
         """
@@ -26,4 +29,30 @@ class ContrastivePCA:
             raise ValueError(f"{num_components} must be positive")
         self.num_components = num_components
     
+    def extract_controls(self, data_path: str, column: str, controls: List[str]) -> Tuple[pd.Index, pd.Index]:
+        """
+        Extracts indices of controls, the non-control indices are targets
+
+        Parameters:
+        ==========
+        data_path: Path to parquet file
+        column: Metadata column in data to obtain indices corresponding to controls
+        controls: List of metadata to be pooled to controls
+
+        Returns:
+        ========
+        (pd.Index, pd.Index)
+            (Control indices, Target indices)
+
+        """
+
+        df = pd.read_parquet(data_path)
+        controls_index_list = [df[df[column] == control].index for control in controls]
+        controls_indices = functools.reduce(lambda x, y: x.union(y), controls_index_list) # lambda is a throwaway function
+        target_indices = df.drop(controls_indices).index
+
+        return Tuple[controls_indices, target_indices]
+
+
     
+
