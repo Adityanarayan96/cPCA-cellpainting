6e43630508528973b50f445c38cdede53126d22b
Adityanarayan96 <adityanarayanoct1997@gmail.com>
2024-10-22 10:07:42 -0500

    "Function add: Added silhouette_label to caclulate normalised silhouette label scores"

--- metrics/silhouette.py
+++ metrics/silhouette.py
@@ -2,6 +2,31 @@ import pandas as pd
 from sklearn.metrics import silhouette_score
 from logging_config import log_method_call
 
+def silhouette_label(data_path: str, column: str, metric: str="cosine", filter_DMSO: bool=True) -> float:
+    """
+    Calculates the normalised Silhouette label score using the silhouette function
+
+    Parameters:
+    ===========
+    data_path: data_path: Path to dataframe.parquet
+    column: Label column for cluster assingment, typically compound or genes
+    metric: Metric used to calculate Silhouette score
+    filter_DMSO: Filter DMSO controls, default is True
+
+    Returns:
+    ========
+    float
+        Normalised Silhoutte label score
+
+    """
+    df = pd.read_parquet(data_path)
+    if filter_DMSO:
+        df = df[df.columns[column]!="DMSO"]
+    
+    return silhouette(df, column, metric=metric)
+
+
+
 @log_method_call
 def silhouette(df: pd.Dataframe, column: str, metric: str="cosine") -> float:
     """
@@ -15,7 +40,8 @@ def silhouette(df: pd.Dataframe, column: str, metric: str="cosine") -> float:
 
     Returns:
     ========
-    Silhouette score (Renormalised between 0 and 1)
+    float
+        Silhouette score (Renormalised between 0 and 1)
 
     """
 
