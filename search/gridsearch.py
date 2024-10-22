e904d9b78d32e2e8f02c2b577a1de8133e4c675c
Adityanarayan96 <adityanarayanoct1997@gmail.com>
2024-10-22 11:44:21 -0500

    "Complete function: Complete function grid_seach to return scores"

--- search/gridsearch.py
+++ search/gridsearch.py
@@ -4,12 +4,15 @@ from metrics.silhouette import silhouette_label, silhouette_batch
 from logging_config import log_method_call
 from cpca.cpca import ContrastivePCA
 
+
 @log_method_call
 def grid_search(data_path: str, 
                 alpha_range: Tuple[float, float, int], 
                 num_components_range: Tuple[int, int, int],
                 column: str="Metadata_JCP2022",
-                contrast: str="DMSO") -> Tuple[float, int]:
+                contrast: str="DMSO",
+                label:str = "Metadata_JCP2022",
+                batch:str = "Metadata_Source") -> Tuple[float, int]:
     """
     Function to implement a grid search on a random subset of samples to tune hyperparameters
 
@@ -20,6 +23,8 @@ def grid_search(data_path: str,
     num_components_range: Three dimensional tuple with start end and number of points for alpha
     column: The column to extract controls based on labels
     contrast: The label value from column for controls
+    label: Labels for score computation
+    batch: Batch for score computation
     
     Returns:
     ========
@@ -44,10 +49,12 @@ def grid_search(data_path: str,
     # Initialize dictionary of scores
     scores ={}
 
-    # # Search over range of grid and populate scores dictionary
-    # for num_components in num_components_search:
-    #     for alpha in alpha_search:
-    #         cpca = ContrastivePCA(data_path, num_components, alpha)
-    #         cpca.contrast(column, contrast)
-    #         scores[(num_components, alpha)] = 
+    # Search over range of grid and populate scores dictionary
+    for num_components in num_components_search:
+        for alpha in alpha_search:
+            cpca = ContrastivePCA(data_path, num_components, alpha)
+            df = cpca.contrast_no_save(column, contrast)
+            scores[(num_components, alpha)] = (silhouette_label(df, label), silhouette_batch(df, label, batch))
+    
+    return scores
             
